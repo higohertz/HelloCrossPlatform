8c478dfd734b3f31b639a985788704faeb7bfcd1
I Khnykin <i.hnykin@socialquantum.com>
2018-06-08 14:25:14 +0300

    [*]

--- make_libs.sh
+++ make_libs.sh
@@ -3,18 +3,21 @@
 DEBUG=1
 GENERATOR= #"Xcode" #"Unix Makefiles"
 
-rm -rf build
-mkdir -p build
-cd build
-
 CMAKE_OPTIONS=()
-CMAKE_OPTIONS+=("-DHH_BUILD_PLATFORM_MACOS=ON")
+CMAKE_OPTIONS+=("-DKI_BUILD_PLATFORM_MACOS=ON")
 if ! [[ -z $DEBUG ]]; then
   CMAKE_OPTIONS+=("-DCMAKE_BUILD_TYPE=Debug")
 else
   CMAKE_OPTIONS+=("-DCMAKE_BUILD_TYPE=Release")
 fi
 
+###############
+
+rm -rf build
+mkdir -p build
+cd build
+
 echo "CMAKE_OPTIONS: ${CMAKE_OPTIONS[@]}"
-cmake ${GENERATOR} ${CMAKE_OPTIONS[@]} ../libs
+cmake ${GENERATOR} ${CMAKE_OPTIONS[@]} ../cmake/lib
+
 make
